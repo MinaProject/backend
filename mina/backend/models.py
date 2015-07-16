4701618d3ec933a75d90763d266dc1ca1bca74b7
codiebeulaine <codiebeulaine@gmail.com>
2015-07-16 08:06:19 +0200

    fixed git error

--- mina/backend/models.py
+++ mina/backend/models.py
@@ -14,7 +14,9 @@ from backend.utils import push_to_git
 import uuid
 from github3 import login
 from github3 import GitHub
-
+from git import Repo
+join = os.path.join
+import hashlib, binascii
 #from git import GitCommandError
 
 
@@ -45,8 +47,12 @@ class Story(models.Model):
 def auto_create_repo(instance, **kwargs):
     try:
         userUUID = uuid.uuid4().hex
-        gh = login('minaglobalfoundation@gmail.com', password='minafoundation15')
-        githubRepo = gh.create_repository(userUUID)
+        gh = GitHub('minaglobalfoundation@gmail.com', password='minafoundation15')
+        githubRepo = gh.create_repository(userUUID, description=u'', homepage=u'', private=False, has_issues=True, has_wiki=True, auto_init=True, gitignore_template=u'')
+        githubRepo.create_blob('hello', 'utf-8')
+        githubRepo.create_commit('first commit', '', '')
+        #origin = githubRepo.create_ref('refs/heads/master', str (hashlib.sha1()))
+        #origin.fetch()
         repoPath = 'repos/' + userUUID
         profile = UserProfile(user=instance, uuid=userUUID)
         EG.init_repo(repoPath, bare=False)
@@ -55,9 +61,11 @@ def auto_create_repo(instance, **kwargs):
             es={'urls': ['http://localhost:9200']})
         #import pdb; pdb.set_trace()
         workspace.repo.create_remote('origin', githubRepo.html_url)
+        print(githubRepo.html_url)
         repo = workspace.repo
         remote = repo.remote()
         remote.fetch()
+        
         remote_master = remote.refs.master
         remote.push(remote_master.remote_head)
     except ValueError:
