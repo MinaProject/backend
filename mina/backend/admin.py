ecf03ae9d72c79bac58f28db4adcce8d915d4297
codiebeulaine <codiebeulaine@gmail.com>
2015-07-23 11:41:37 +0200

    deleted dupliacte crap

--- mina/backend/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Story
-from.models import UserProfile
-admin.site.register(Story)
-admin.site.register(UserProfile)
