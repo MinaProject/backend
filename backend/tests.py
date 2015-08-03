999e050788bf82b2211effdb5392f53f6885f0ae
codiebeulaine <codiebeulaine@gmail.com>
2015-08-03 12:20:35 +0200

    changing tet

--- backend/tests.py
+++ backend/tests.py
@@ -1,17 +1,8 @@
 from django.contrib.auth.models import User
 from backend.base import BaseTestCase
-from django.test.client import Client
 
 
 class TestModels(BaseTestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_superuser(
-            'testuser', 'test@gmail.com', password='test')
-        self.client = Client()
-        self.client.login(username='testuser', password='test')
-
-        self.workspace = self.mk_workspace()
 
     def test_user_info(self):
         user = User.objects.create_user('foo', 'foo@example.org', 'bar')
