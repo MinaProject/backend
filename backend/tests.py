96b4866f0b837be2de1c10208c779a820b8cf333
codiebeulaine <codiebeulaine@gmail.com>
2015-09-13 12:48:54 +0200

    fixing pep8

--- backend/tests.py
+++ backend/tests.py
@@ -1,11 +1,7 @@
-from backend.base import BaseTestCase
 from mock import patch
 from views import create_user
-import json
-from django.http import HttpResponse
 from django.http import HttpRequest
 import unittest
-import requests
 from django.contrib.auth.models import User
 
 
@@ -19,5 +15,5 @@ class TestModels(unittest.TestCase):
                         "username": 'foobar',
                         "password": 'foobar'}
         with patch.object(User.objects, 'create_user') as get_mock:
-        	get_mock.return_value.body == 'not created'
-        	assert create_user(request).body == 'created'
+            get_mock.return_value.body == 'not created'
+            assert create_user(request).body == 'created'
