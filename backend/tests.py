495eb0d0709e028938ba5b25ba683bbc31e900d5
codiebeulaine <codiebeulaine@gmail.com>
2015-09-21 12:08:32 +0200

    mockery

--- backend/tests.py
+++ backend/tests.py
@@ -39,7 +39,19 @@ class TestCRUD(unittest.TestCase):
         assert create_story(request).body == 'created'
 
         # test view all stories
-        with patch.object(elasticgit.workspace.Workspace, 'pull'):
+        with patch.object(elasticgit.workspace.Workspace, 'pull',
+                          return_value='{"category": 1, "body": "f '
+                          'oobar", "uuid": "e1f883784c874461884e7bf'
+                          'd4180d325", "author": "a8230aca7b3943edbb'
+                          'e4e89755450fbb", "title": "foo", "_versio'
+                          'n": {"package_version": "1.4.1", "langua'
+                          'ge_version": "2.7.10", "language": "pyth'
+                          'on", "language_version_string": "2.7.10 '
+                          '(default, Jul 14 2015, 19:46:27)[GCC '
+                          '4.2.1 Compatible Apple LLVM 6.0 (clang-6'
+                          '  00.0.39)]", "package": "elastic-git"},'
+                          '   "co_authors": null, "update_count": n'
+                          '   ull}'):
             assert stories(request) != []
 
         # test view specific story
@@ -48,17 +60,53 @@ class TestCRUD(unittest.TestCase):
         request = HttpRequest()
         request.method = 'POST'
         request.POST = storyUUID
-        with patch.object(elasticgit.workspace.Workspace, 'pull'):
+        with patch.object(elasticgit.workspace.Workspace, 'pull',
+                          return_value='{"category": 1, "body": "f '
+                          'oobar", "uuid": "e1f883784c874461884e7bf'
+                          'd4180d325", "author": "a8230aca7b3943edbb'
+                          'e4e89755450fbb", "title": "foo", "_versio'
+                          'n": {"package_version": "1.4.1", "langua'
+                          'ge_version": "2.7.10", "language": "pyth'
+                          'on", "language_version_string": "2.7.10 '
+                          '(default, Jul 14 2015, 19:46:27)[GCC '
+                          '4.2.1 Compatible Apple LLVM 6.0 (clang-6'
+                          '  00.0.39)]", "package": "elastic-git"},'
+                          '   "co_authors": null, "update_count": n'
+                          '   ull}'):
             assert view_story(request).body != 'story not found'
 
         # test view all category stories
         request.POST = story.category
-        with patch.object(elasticgit.workspace.Workspace, 'pull'):
+        with patch.object(elasticgit.workspace.Workspace, 'pull',
+                          return_value='{"category": 1, "body": "f '
+                          'oobar", "uuid": "e1f883784c874461884e7bf'
+                          'd4180d325", "author": "a8230aca7b3943edbb'
+                          'e4e89755450fbb", "title": "foo", "_versio'
+                          'n": {"package_version": "1.4.1", "langua'
+                          'ge_version": "2.7.10", "language": "pyth'
+                          'on", "language_version_string": "2.7.10 '
+                          '(default, Jul 14 2015, 19:46:27)[GCC '
+                          '4.2.1 Compatible Apple LLVM 6.0 (clang-6'
+                          '  00.0.39)]", "package": "elastic-git"},'
+                          '   "co_authors": null, "update_count": n'
+                          '   ull}'):
             assert view_category_stories(request) != 'story not found'
 
         # test view all user stories
         request.POST = story.uuid
-        with patch.object(elasticgit.workspace.Workspace, 'pull'):
+        with patch.object(elasticgit.workspace.Workspace, 'pull',
+                          return_value='{"category": 1, "body": "f '
+                          'oobar", "uuid": "e1f883784c874461884e7bf'
+                          'd4180d325", "author": "a8230aca7b3943edbb'
+                          'e4e89755450fbb", "title": "foo", "_versio'
+                          'n": {"package_version": "1.4.1", "langua'
+                          'ge_version": "2.7.10", "language": "pyth'
+                          'on", "language_version_string": "2.7.10 '
+                          '(default, Jul 14 2015, 19:46:27)[GCC '
+                          '4.2.1 Compatible Apple LLVM 6.0 (clang-6'
+                          '  00.0.39)]", "package": "elastic-git"},'
+                          '   "co_authors": null, "update_count": n'
+                          '   ull}'):
             assert view_user_stories(request) != 'story not found'
 
         # test view user information
